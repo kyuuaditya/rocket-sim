c20682d36c98ffc85832f20258138b9238e2f1b9
Aditya Pratap Singh <thisisaditya.me@gmail.com>
2025-04-24 02:00:55 +0530

    final changes and some minor fixes

--- main.cpp
+++ main.cpp
@@ -3,13 +3,16 @@
 #include <cmath>
 #include <sfml/Graphics.hpp>
 
-bool visual = true; // Set to true for visual simulation,
-bool write = true; // set to true to write to CSV
+bool visual = false; // Set to true for visual simulation,
+bool write = false; // set to true to write to CSV
+bool write_liftoff = true;
+double target_velocity = 11000;
+double braking_distance_s3 = 2000 * 1e3; // 2000 km
 
 // Function to write data to a CSV file
 std::ofstream file("filename.csv");
 void writeDataToCSV(double a, double b, double c) {
-    if (write) {
+    if (write || write_liftoff) {
         if (file.is_open()) {
             file << a << " " << b << " " << c << "\n";
         }
@@ -40,7 +43,7 @@ int main() {
     // rocket data
     const long double enginePower = 50.0e10; // watts
     long double rocketTotalMass = 2.8e6; // kg
-    long double rocketDryMass = 6.0e5; // kg
+    long double rocketDryMass = 4.0e5; // kg
     long double initialFuelMass = rocketTotalMass - rocketDryMass;
     long double currentFuelMass = initialFuelMass;
     long double currentRocketMass = currentFuelMass + rocketDryMass;
@@ -53,7 +56,7 @@ int main() {
     long double x = 0; // displacement initially set to 0
     long double rocketVelocity = 0;
     long double acceleration = 0;
-    long double timeExpended = 0;
+    long double timeExpended_1 = 0;
     long double exhaustVelocity = 0;
     long double thrust = 0;
     long double netForce = 0;
@@ -173,8 +176,13 @@ int main() {
 
 
     int count = 0; // counter for simulation steps
+    double work = 0; // cost of the simulation
+    double timeEfficiencyConstant = 1; // time efficiency of the simulation
+    double fuelEfficiencyConstant = 1; // fuel efficiency of the simulation
+    double time_cost_1 = 0;
+
     // -------------------------------------------------------------- State 1 ----------------------------------------------------------
-    while (rocketVelocity < 15000) { // Run for 5 minutes
+    while (rocketVelocity < target_velocity) { // Run for 5 minutes
         sf::Event event; // event catcher to close window
         while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed) {
@@ -217,11 +225,13 @@ int main() {
 
         // update rocket velocity and position
         long double distanceInTimeStep = rocketVelocity * timeStep + 0.5 * acceleration * pow(timeStep, 2);
+        work += distanceInTimeStep * netForce;
         rocketVelocity += acceleration * timeStep;
         x += distanceInTimeStep;
-        timeExpended += 1;
+        timeExpended_1 += 1;
+        time_cost_1 += timeStep / distanceInTimeStep;
 
-        if (count % 1000 == 0 && write) {
+        if (count % 1000 == 0 && (write || write_liftoff)) {
             writeDataToCSV(count / 1000, x, rocketVelocity);
         }
 
@@ -234,7 +244,7 @@ int main() {
                 text_rocket_velocity.setString("Rocket Speed: " + std::to_string(rocketVelocity) + " m/s");
                 text_rocket_acceleration.setString("Rocket Acceleration: " + std::to_string(acceleration) + " m/s^2");
                 text_rocket_fuel.setString("Rocket Fuel: " + std::to_string(currentFuelMass) + " kg");
-                text_rocket_time.setString("Rocket Time: " + std::to_string(timeExpended * timeStep) + " seconds");
+                text_rocket_time.setString("Rocket Time: " + std::to_string(timeExpended_1 * timeStep) + " seconds");
                 text_rocket_displacement.setString("Rocket Distance: " + std::to_string(x / 1e3) + " km");
                 text_rocket_cfmi.setString("Current Fuel Mass Input: " + std::to_string(currentEFMI));
                 text_simulation_phase.setString("Liftoff!");
@@ -259,15 +269,22 @@ int main() {
         count++;
     }
     // normalize timeExpended to seconds
-    timeExpended *= timeStep;
+    timeExpended_1 *= timeStep;
+    time_cost_1 /= 1e3; // normalize time cost to seconds/km
+    time_cost_1 += 1e3 / sqrt(rocketVelocity);
+    double fuel_cost_1 = (initialFuelMass - currentFuelMass) / work;
+    double cost_liftoff = time_cost_1 * timeEfficiencyConstant + fuel_cost_1 * fuelEfficiencyConstant + time_cost_1 * fuel_cost_1; // cost of the simulation
 
     std::cout << "------------------------------------------------Stage 1 Complete----------------------------------------------" << std::endl;
-    std::cout << "Time: " << timeExpended << " seconds\n";
+    std::cout << "Time: " << timeExpended_1 << " seconds\n";
     std::cout << "Final velocity: " << rocketVelocity << " m/s\n";
     std::cout << "Distance traveled: " << x / 1e3 << " km\n";
     std::cout << "Distance Left: " << (d - x) / 1e3 << "km" << std::endl;
     std::cout << "Current Rocket Mass: " << currentRocketMass << " kg\n";
     std::cout << "Fuel consumed: " << initialFuelMass - currentFuelMass << " kg " << "Fuel remaining: " << currentFuelMass << " kg\n";
+    std::cout << "Fuel Cost: " << fuel_cost_1 << " Kg/j\n";
+    std::cout << "Time Cost: " << time_cost_1 << " s/km\n";
+    std::cout << "Cost of the simulation: " << cost_liftoff << std::endl;
 
     // -------------------------------------------------------------- State 2 ----------------------------------------------------------
     // Load background texture
@@ -294,10 +311,12 @@ int main() {
     double distanceCruised = 0;
 
     count = 0;
-    timeExpended = 0;
+    double timeExpended_2 = 0;
+    double time_cost_2 = 0;
+    bool rocketStatus = true; // Rocket status
 
     timeStep = 1; // in seconds
-    while (distanceCruised + 5000 * 1e3 < distanceToCruise && cruiseVelocity > 0) { // 30 hours for reference
+    while (distanceCruised + braking_distance_s3 < distanceToCruise && cruiseVelocity > 0) { // 30 hours for reference
         sf::Event event; // event catcher to close window
         while (window.pollEvent(event)) {
             if (event.type == sf::Event::Closed) {
@@ -320,7 +339,9 @@ int main() {
         cruiseVelocity += acceleration * timeStep;
         x += distanceInTimeStep;
         distanceCruised += distanceInTimeStep;
-        timeExpended += 1;
+        timeExpended_2 += 1;
+
+        time_cost_2 += timeStep / distanceInTimeStep; // time cost for the simulation
 
         if (write) {
             writeDataToCSV(count, x, cruiseVelocity);
@@ -335,7 +356,7 @@ int main() {
                 text_rocket_velocity.setString("Rocket Speed: " + std::to_string(cruiseVelocity) + " m/s");
                 text_rocket_acceleration.setString("Rocket Acceleration: " + std::to_string(acceleration) + " m/s^2");
                 text_rocket_fuel.setString("Rocket Fuel: " + std::to_string(currentFuelMass) + " kg");
-                text_rocket_time.setString("Rocket Time: " + std::to_string(timeExpended * timeStep) + " seconds");
+                text_rocket_time.setString("Rocket Time: " + std::to_string(timeExpended_2 * timeStep) + " seconds");
                 text_rocket_displacement.setString("Rocket Distance: " + std::to_string(x / 1e3) + " km");
                 text_simulation_phase.setString("Cruiseing!");
 
@@ -349,7 +370,6 @@ int main() {
                 window.draw(rocket);
                 // sf::sleep(sf::milliseconds(1)); // Sleep for 1 millisecond to control the frame rate
 
-
                 window.display();
             }
         }
@@ -357,12 +377,23 @@ int main() {
         count++;
     }
 
+    if (cruiseVelocity < 0) {
+        std::cout << "Rocket has stopped moving" << std::endl;
+        currentEFMI = 0; // Stop engine if fuel runs out
+        thrust = 0;
+        rocketStatus = false; // Stop the rocket
+    }
+
+
     std::cout << "------------------------------------------------Stage 2 Complete----------------------------------------------" << std::endl;
-    std::cout << "Time: " << timeExpended << " seconds\n";
+    std::cout << "Time: " << timeExpended_2 << " seconds\n";
     std::cout << "Cruise velocity: " << cruiseVelocity << " m/s\n";
     std::cout << "Distance traveled: " << distanceCruised / 1e3 << " km\n";
     std::cout << "Total Distance traveled: " << (x) / 1e3 << " km\n";
-    std::cout << "Distance Left: " << (d - x) / 1e3 << "km" << std::endl;;
+    std::cout << "Distance Left: " << (d - x) / 1e3 << "km" << std::endl;
+    std::cout << "time cost: " << time_cost_2 << " s/km\n";
+    double cost_cruise = time_cost_2;
+
     // -------------------------------------------------------------- State 3 ----------------------------------------------------------
     // update rocket sprite
     if (!rocketTexture.loadFromFile("assets/texture/rocket.png")) { // Replace with your rocket image path
@@ -381,9 +412,14 @@ int main() {
     thrust = 0;
     netForce = 0;
     currentEFMI = 0;
+    double rocketMassPostCruise = currentFuelMass;
 
-    timeExpended = 0;
+    double timeExpended_3 = 0;
+
     count = 0;
+    work = 0;
+    double time_cost_3 = 0; // time cost for the simulation
+    double fuel_cost_3 = 0; // fuel cost for the simulation
 
     timeStep = 0.001; // in seconds
 
@@ -427,7 +463,9 @@ int main() {
         rocketVelocity += acceleration * timeStep;
         x += distanceInTimeStep;
         distanceDecelerated += distanceInTimeStep;
-        timeExpended += 1;
+        timeExpended_3 += 1;
+        work += -distanceInTimeStep * netForce;
+        time_cost_3 += timeStep / distanceInTimeStep; // time cost for the simulation
 
         // change efmi as per goal final velocity ie. 100 m/s
         if ((-(pow(rocketVelocity, 2) - pow(100, 2)) / (2 * (distancetoDecelerate - 1 * 1e3 - distanceDecelerated)) < acceleration) && acceleration > -50) {
@@ -439,7 +477,7 @@ int main() {
         if (acceleration < -50 && currentEFMI >= 1) currentEFMI -= 1; // safety check for 5g
 
 
-        if (count % 1000 == 0) {
+        if (count % 1000 == 0 && write) {
             writeDataToCSV(count / 1000, x, rocketVelocity);
         }
 
@@ -451,7 +489,7 @@ int main() {
                 text_rocket_velocity.setString("Rocket Speed: " + std::to_string(rocketVelocity) + " m/s");
                 text_rocket_acceleration.setString("Rocket Acceleration: " + std::to_string(acceleration) + " m/s^2");
                 text_rocket_fuel.setString("Rocket Fuel: " + std::to_string(currentFuelMass) + " kg");
-                text_rocket_time.setString("Rocket Time: " + std::to_string(timeExpended * timeStep) + " seconds");
+                text_rocket_time.setString("Rocket Time: " + std::to_string(timeExpended_3 * timeStep) + " seconds");
                 text_rocket_displacement.setString("Rocket Distance: " + std::to_string(x / 1e3) + " km");
                 text_rocket_cfmi.setString("Current Fuel Mass Input: " + std::to_string(currentEFMI));
                 text_simulation_phase.setString("decelerating!");
@@ -474,15 +512,29 @@ int main() {
 
         count++;
     }
-    timeExpended *= timeStep;
+    if (rocketVelocity < 0) {
+        time_cost_3 = 1e3;
+        fuel_cost_3 = 1e3;
+    }
+    else {
+        time_cost_3 /= 1e3; // normalize time cost to seconds/km
+        time_cost_3 += rocketVelocity / 100; // time cost for the simulation
+        fuel_cost_3 += (rocketMassPostCruise - currentFuelMass) / work; // fuel cost for the simulation
+    }
+    timeExpended_3 *= timeStep;
+    double cost_decelerate = time_cost_3 * timeEfficiencyConstant + fuel_cost_3 * fuelEfficiencyConstant + time_cost_3 * fuel_cost_3; // cost of the simulation
+
 
     std::cout << "------------------------------------------------Stage 3 Complete----------------------------------------------" << std::endl;
-    std::cout << "Time: " << timeExpended << " seconds\n";
+    std::cout << "Time: " << timeExpended_3 << " seconds\n";
     std::cout << "Cruise velocity: " << rocketVelocity << " m/s\n";
     std::cout << "Distance traveled: " << distanceDecelerated / 1e3 << " km\n";
     std::cout << "Total Distance traveled: " << (x) / 1e3 << " km\n";
     std::cout << "Fuel consumed: " << initialFuelMass - currentFuelMass << " kg " << "Fuel remaining: " << currentFuelMass << " kg\n";
-    std::cout << "Distance Left: " << (d - x) << "m" << std::endl;;
+    std::cout << "Distance Left: " << (d - x) << "m\n";
+    std::cout << "time cost: " << time_cost_3 << " s/km\n";
+    std::cout << "Fuel Cost: " << fuel_cost_3 << " Kg/j\n";
+    std::cout << "Cost of the simulation: " << cost_decelerate << std::endl;
     // -------------------------------------------------------------- State 4 ----------------------------------------------------------
     // initialize rocket statistics
     distancetoDecelerate = d - x;
@@ -493,13 +545,16 @@ int main() {
     distanceDecelerated = 0;
     double landing_range = 1; // 1 meter
     bool landing_phrase = true;
+    double time_cost_4 = 0; // time cost for the simulation
+    double fuel_cost_4 = 0; // fuel cost for the simulation
+    double rocketMassPostDecelerating = currentFuelMass;
 
     count = 0;
-    timeExpended = 0;
+    double timeExpended_4 = 0;
 
     timeStep = 0.001; // in seconds
 
-    while (distancetoDecelerate - landing_range > distanceDecelerated && x < d && landing_phrase) {
+    while (distancetoDecelerate - landing_range > distanceDecelerated && x < d && landing_phrase && rocketStatus) {
         if (count * timeStep > 100) { // 100 seconds allowed for landing
             break;
         }
@@ -545,7 +600,9 @@ int main() {
         x += distanceInTimeStep;
 
         distanceDecelerated += distanceInTimeStep;
-        timeExpended += 1;
+        timeExpended_4 += 1;
+        work += -distanceInTimeStep * netForce;
+        time_cost_4 += timeStep / distanceInTimeStep; // time cost for the simulation
 
         if (-(pow(rocketVelocity, 2)) / (2 * (distancetoDecelerate - landing_range - distanceDecelerated)) < acceleration) { // 0 meter per second is the goal velocity
             currentEFMI += 1;
@@ -572,7 +629,7 @@ int main() {
             rocket.setTexture(&rocketTexture);
         }
 
-        if (count % 1000 == 0) {
+        if (count % 1000 == 0 && write) {
             writeDataToCSV(count / 1000, x, rocketVelocity);
         }
 
@@ -584,7 +641,7 @@ int main() {
                 text_rocket_velocity.setString("Rocket Speed: " + std::to_string(rocketVelocity) + " m/s");
                 text_rocket_acceleration.setString("Rocket Acceleration: " + std::to_string(acceleration) + " m/s^2");
                 text_rocket_fuel.setString("Rocket Fuel: " + std::to_string(currentFuelMass) + " kg");
-                text_rocket_time.setString("Rocket Time: " + std::to_string(timeExpended * timeStep) + " seconds");
+                text_rocket_time.setString("Rocket Time: " + std::to_string(timeExpended_4 * timeStep) + " seconds");
                 text_rocket_displacement.setString("Rocket Distance: " + std::to_string(x / 1e3) + " km");
                 text_rocket_cfmi.setString("Current Fuel Mass Input: " + std::to_string(currentEFMI));
                 text_simulation_phase.setString("Landing!");
@@ -606,15 +663,36 @@ int main() {
         }
         count++;
     }
+    if (rocketVelocity < 0) {
+        time_cost_4 = 1e3;
+        fuel_cost_4 = 1e3;
+    }
+    else {
+        // normalize timeExpended to seconds
+        time_cost_4 /= 1e3; // normalize time cost to seconds/km
+        time_cost_4 += rocketVelocity; // time cost for the simulation
+        fuel_cost_4 += (rocketMassPostDecelerating - currentFuelMass) / work; // fuel cost for the simulation
+    }
+    double cost_landing = time_cost_4 * timeEfficiencyConstant + fuel_cost_4 * fuelEfficiencyConstant + time_cost_4 * fuel_cost_4; // cost of the simulation
 
-    timeExpended *= timeStep; // normalize timeExpended to seconds
+    timeExpended_4 *= timeStep; // normalize timeExpended to seconds
     std::cout << "------------------------------------------------Stage 4 Complete----------------------------------------------" << std::endl;
-    std::cout << "Time: " << timeExpended << " seconds\n";
+    std::cout << "Time: " << timeExpended_4 << " seconds\n";
     std::cout << "Cruise velocity: " << rocketVelocity << " m/s\n";
     std::cout << "Distance traveled: " << distanceDecelerated / 1e3 << " km\n";
     std::cout << "Total Distance traveled: " << (x) / 1e3 << " km\n";
     std::cout << "Fuel consumed: " << initialFuelMass - currentFuelMass << " kg " << "Fuel remaining: " << currentFuelMass << " kg\n";
-    std::cout << "Distance Left: " << (d - x) << "m" << std::endl;;
+    std::cout << "Distance Left: " << (d - x) << "m" << std::endl;
+    std::cout << "time cost: " << time_cost_4 << " s/km\n";
+    std::cout << "Fuel Cost: " << fuel_cost_4 << " Kg/j\n";
+    std::cout << "Cost of the simulation: " << cost_landing << std::endl;
+    std::cout << "------------------------------------------------stats----------------------------------------------\n" << std::endl;
+    std::cout << "Total Time: " << timeExpended_1 + timeExpended_2 + timeExpended_3 + timeExpended_4 << " seconds\n";
+    std::cout << "Total Distance: " << (x) / 1e3 << " km\n";
+
+    std::cout << "total time cost: " << time_cost_1 + time_cost_2 + time_cost_3 + time_cost_4 << " s/km\n";
+    std::cout << "total fuel cost: " << fuel_cost_1 + fuel_cost_3 + fuel_cost_4 << " Kg/j\n";
+    std::cout << "total cost of the simulation: " << cost_liftoff + cost_cruise + cost_decelerate + cost_landing << std::endl;
 
     while (visual && window.isOpen()) {
         sf::Event event; // event catcher to close window
